f138c91fc105ee41a5cce7461a55998f29d4bbcb
bigstone <dssong019980106@gmail.com>
2021-03-08 21:03:22 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {makeStyles} from '@material-ui/core/styles';
+import Header from './components/Header';
+import Routes from './Router';
+import Footer from './components/Footer';
+import SwiperCore, {Autoplay, Navigation, Pagination, A11y} from 'swiper';
 
+// Import Swiper styles
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+import 'swiper/components/scrollbar/scrollbar.min.css';
+
+// Import Swiper styles
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%',
+    overflowX: 'hidden',
+  },
+  main: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    height: '100vh',
+  },
+  imageSection: {
+    backgroundColor: '#cfe8fc',
+  },
+}));
 function App() {
+  const classes = useStyles();
+  SwiperCore.use([Autoplay, Navigation, Pagination, A11y]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
+      <div className={classes.main}>
+        <Routes />
+      </div>
+      <Footer
+        title="국민독서문화진흥회"
+        description="등록번호: 000-00-000000 | 회장: 김을호 | 주소: 서울시 성북구 동소문로 269 601호(길음동, 태창빌딩) | 대표전화: 02-913-9582 | 팩스: 02-913-9584"
+      />
     </div>
   );
 }
